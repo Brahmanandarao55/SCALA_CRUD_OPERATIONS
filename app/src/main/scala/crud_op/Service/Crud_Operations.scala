94d59d09fd1385cdb861fe68a74eeb1122891e3e
Brahmananda <brindharao5@gmail.com>
2024-07-16 14:12:58 +0530

    Predevelopment-2

--- app/src/main/scala/crud_op/Service/Crud_Operations.scala
+++ app/src/main/scala/crud_op/Service/Crud_Operations.scala
@@ -5,6 +5,7 @@ import crud_op.Entity.Person
 import crud_op.Repository.DataBaseRepoImpl
 import org.slf4j.{Logger, LoggerFactory}
 
+import java.io.EOFException
 import scala.io.StdIn
 
 class Crud_Operations {
@@ -14,61 +15,72 @@ class Crud_Operations {
   val logger: Logger = LoggerFactory.getLogger(getClass)
 
   def menu(): Unit = {
-
-    logger.info("Choose an operation:")
-    logger.info("1. Create Table")
-    logger.info("2. Insert Data into Table")
-    logger.info("3. Get Data by ID")
-    logger.info("4. Update Data into Table")
-    logger.info("5. Generate CSV file Data From DataBase")
-    logger.info("6. Delete Data by ID")
-    logger.info("7. Exit")
-    logger.info("To Repeat Options Enter Value Greater Than 6")
+    val menuOptions =
+      """
+    Choose an operation:
+    1. Create Table
+    2. Insert Data into Table
+    3. Get Data by ID
+    4. Update Data into Table
+    5. Generate CSV file Data From DataBase
+    6. Delete Data by ID
+    7. Exit
+    To Repeat Options Enter Value Greater Than 7
+  """
+    logger.info(menuOptions)
     logger.info("Enter your choice:")
-    val input = StdIn.readInt()
-    input match {
-      case 1 => logger.info(repo.createTable())
-      case 2 =>
-        logger.info("Insertion Begin")
-        repo.insertData(filePath)
-        logger.info("Insertion Ended")
-      case 3 =>
-        logger.info("Enter ID: ")
-        val id = StdIn.readInt()
-        repo.getDataById(Some(id))
-      case 4 =>
-        logger.info("Enter Updating details:")
-        logger.info("Enter Name: ")
-        val name = StdIn.readLine()
-        logger.info("Enter Age: ")
-        val age = StdIn.readInt()
-        logger.info("Enter Salary: ")
-        val salary = StdIn.readInt()
-        logger.info("Enter Profession: ")
-        val profession = StdIn.readLine()
-        logger.info("Enter Location: ")
-        val location = StdIn.readLine()
-        logger.info("Enter the Rank: ")
-        val rank = StdIn.readFloat()
-        logger.info("Enter the Block: ")
-        val block = StdIn.readChar()
-        logger.info("Enter Updating ID: ")
-        val id = StdIn.readInt()
-        repo.updateData(Person(name, age, salary, profession, location, rank, block), id)
+    try {
+      val input = StdIn.readLine().toInt
 
-      case 5 =>
-        repo.getAll
-      case 6 =>
-        logger.info("Enter deleting ID")
-        val id = StdIn.readInt()
-        repo.deleteData(Some(id))
-      case 7 =>
-        logger.info("Invalid Choice")
-      case _ =>
-        logger.info("Invalid choice. Please try again.")
-        menu()
+      input match {
+        case 1 => logger.info(repo.createTable())
+        case 2 =>
+          logger.info("Insertion Begin")
+          repo.insertData(filePath)
+          logger.info("Insertion Ended")
+        case 3 =>
+          logger.info("Enter ID: ")
+          val id = StdIn.readInt()
+          repo.getDataById(Some(id))
+        case 4 =>
+          logger.info("Enter Updating details:")
+          logger.info("Enter Name: ")
+          val name = StdIn.readLine()
+          logger.info("Enter Age: ")
+          val age = StdIn.readInt()
+          logger.info("Enter Salary: ")
+          val salary = StdIn.readInt()
+          logger.info("Enter Profession: ")
+          val profession = StdIn.readLine()
+          logger.info("Enter Location: ")
+          val location = StdIn.readLine()
+          logger.info("Enter the Rank: ")
+          val rank = StdIn.readFloat()
+          logger.info("Enter the Block: ")
+          val block = StdIn.readChar()
+          logger.info("Enter Updating ID: ")
+          val id = StdIn.readInt()
+          repo.updateData(Person(name, age, salary, profession, location, rank, block), id)
 
+        case 5 =>
+          repo.getAll
+        case 6 =>
+          logger.info("Enter deleting ID")
+          val id = StdIn.readInt()
+          repo.deleteData(Some(id))
+        case 7 =>
+          logger.info("Invalid Choice")
+        case _ =>
+          logger.info("Invalid choice. Please try again.")
+          menu()
 
+      }
+    }
+    catch {
+      case e: EOFException => e.printStackTrace()
     }
   }
+
+
 }
+
